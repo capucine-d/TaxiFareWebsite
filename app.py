8abe75b50f0f71b0612b4654ca8338b0ebb13d01
Capucine Dehaut <capucinedehaut@gmail.com>
2021-02-19 15:53:56 +0100

    create app

--- app.py
+++ app.py
@@ -1,48 +1,58 @@
 import streamlit as st
-
+import datetime
+import requests
+import pandas as pd
 '''
-# TaxiFareModel front
+# Welcome to Taxi Calculator
 '''
 
 st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+Get an estimate of the price you'll pay for your taxi
 ''')
 
 '''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
+##  Please enter your trip information:
 '''
+date = st.date_input("Select a date", datetime.date(2021, 2, 19))
 
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
+time = st.time_input('Select a time', datetime.time(16, 00))
 
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
 
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+pickup_longitude = st.text_input('Enter pickup longitude', '40.7128')
+pickup_latitude = st.text_input('Enter pickup latitude', '74.0060')
 
-url = 'http://taxifare.lewagon.ai/predict_fare/'
+dropoff_longitude = st.text_input('Enter dropoff longitude', '40.7128')
+dropoff_latitude = st.text_input('Enter dropoff latitude', '74.0060')
 
-if url == 'http://taxifare.lewagon.ai/predict_fare/':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
+passenger_count = st.number_input('Insert a number of passengers')
 
 '''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
+##  Trip Summary:
 '''
+st.write('Your trip will take place on ', date,
+         'at ',time, 'with', int(passenger_count) ,'passenger(s)')
+st.write('Pickup location: ', '(', pickup_longitude,' ,',pickup_latitude,')')
+st.write('Dropoff location: ', '(', dropoff_longitude,' ,',dropoff_latitude,')')
+
+url = 'https://api-taxifare-5sppn64aja-ew.a.run.app/predict_fare'
+#url = 'http://localhost:8000/predict_fare'
+
+
+params = {
+        'key': 'key',
+        'pickup_datetime': f'{date} {time} UTC',
+        'pickup_longitude': pickup_longitude,
+        'pickup_latitude': pickup_latitude,
+        'dropoff_longitude': dropoff_longitude,
+        'dropoff_latitude': dropoff_latitude,
+        'passenger_count': int(passenger_count)
+        }
+
+response = requests.get(url, params)
+prediction = response.json()['prediction']
+
+'''
+## Find out your taxi fare
+'''
+
+st.write('Predicted fare:', prediction)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content if 'git+' not in x]
+
+setup(name='TaxiFareWebsite',
+      packages=find_packages(),
+      # include_package_data: to install data from MANIFEST.in
+      include_package_data=True,
+      install_requires=requirements)
